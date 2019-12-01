1ee2e380a78d921c70f0177a5f0231c82a6555f2
zlj824635819 <zlj824635819>
2019-12-01 15:32:15 +0800

    new

---
+++ 面试练习/ThreadTest/src/main/java/org/logan/App.java
@@ -0,0 +1,13 @@
+package org.logan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
